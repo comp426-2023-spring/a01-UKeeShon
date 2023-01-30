a8faf6c54ab363bd97fdf3e288af5202ff6212cc
UKeeShon <ysheng04@email.unc.edu>
2023-01-29 22:57:22 -0500

    12

--- server.js
+++ server.js
@@ -5,18 +5,18 @@ const minimist = require('minimist');
 const argv = minimist(process.argv.slice(2));
 const port = argv.port == undefined ? 3000 : argv.port;
 
-fs.readFile('./public/index.html', (err, data) => {
+fs.readFile('./public/index.html','utf-8', (err, data) => {
 	if (err) {
 		console.error(err);
 		return;
 	}
+}
 
-	const server = http.createServer((req, res) => {
-		res.statusCode = 200;
-		res.setHeader('Content-Type', 'text/html');
-		res.end(data);
-	});
-	server.listen(port, () => {
-		console.log('Server listening on port ${port}');
-	});
+const server = http.createServer((req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/html');
+	res.end(data);
 });
+
+server.listen(port)
+console.log(`Server listening on port ${port}`);
